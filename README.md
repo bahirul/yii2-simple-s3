0066792646819d1d02a64dc1e92186777e6a8afa
m24 <rizki.device@gmail.com>
2020-04-30 14:34:47 +0800

    init

---
+++ README.md
@@ -0,0 +1,123 @@
+# Simple Custom S3 for Yii2
+
+Simple extension for custom S3 filesystem with basic functionality : read, write, delete.
+This extension use [Flysystem](http://flysystem.thephpleague.com/) as core filesystem.
+
+## Installation
+
+The preferred way to install this extension is through [composer](http://getcomposer.org/download/).
+
+Either run
+
+```bash
+$ composer require bahirul/yii2-s3-custom
+```
+
+or add
+
+```
+"bahirul/yii2-s3-custom": "~0.0.1"
+```
+
+to the `require` section of your `composer.json` file.
+
+## Configuring
+
+Configure application `components` as follows
+
+```php
+return [
+    //...
+    'components' => [
+        //...
+        's3' => [
+            'class' => 'bahirul\s3\Yii2SimpleS3',
+            'endpoint' => 'YOUR_S3_URL',
+            'key'    => 'YOUR_S3_KEY',
+            'secret' => 'YOUR_S3_SECRET',
+            'region' => 'YOUR_S3_REGION',
+            'version' => 'YOUR_S3_VERSION',
+            'bucket' => 'YOUR_S3_BUCKET',
+        ],
+    ],
+];
+```
+
+## Usage
+
+### Read file from S3
+
+`readFile()` method will return array with key 'stream' and 'mimetype'.
+
+send as response in controller
+
+```php
+// s3 custom init
+$s3 = Yii::$app->s3;
+
+// your requested file
+$fileRequest = 'example_file_image.jpg';
+
+// read file on S3
+$readFile = $s3->readFile($fileRequest);
+
+// init web response
+$response = Yii::$app->response;
+$response->format = \yii\web\Response::FORMAT_RAW;
+$response->stream = $readFile['stream'];
+
+return $response->send();
+```
+
+or send as file in controller
+
+```php
+// s3 custom init
+$s3 = Yii::$app->s3;
+
+// your requested file
+$fileRequest = 'example_file_image.jpg';
+
+// read file on S3
+$readFile = $s3->readFile($fileRequest);
+
+// init web response
+$response = Yii::$app->response;
+
+return $response->sendStreamAsFile($readFile['stream'], $fileRequest ,['mimeType' => $readFile['mimetype']]);
+```
+
+### Write file to S3
+
+`writeFile()` method will return boolean.
+
+write (upload) usage on controller
+
+```php
+// your file upload instance
+$fileModel = UploadedFile::getInstance($model, 'attributeName');
+
+$s3 = Yii::$app->s3;
+
+// generate random filename
+$filename = time() . '.jpg';
+
+if($model->load($post) && $model->validate()){
+    // upload file to s3
+    $s3->writeFile($fileModel, $filename);
+}
+```
+
+### Delete file on S3
+
+`deleteFile()` method will return boolean.
+
+delete usage on controller
+
+```php
+$s3 = Yii::$app->s3;
+
+$fileRequest = 'example_file_image.jpg';
+
+$s3->deleteFile($fileRequest);
+```
---
+++ src/Yii2SimpleS3.php
@@ -0,0 +1,134 @@
+<?php
+namespace bahirul\s3;
+
+use yii\base\Component;
+use yii\base\InvalidConfigException;
+use yii\base\InvalidArgumentException;
+use yii\web\NotFoundHttpException;
+
+use Aws\S3\S3Client;
+use League\Flysystem\AwsS3v3\AwsS3Adapter;
+use League\Flysystem\Filesystem;
+
+class Yii2SimpleS3 extends Component
+{
+	public $endpoint;
+
+	public $key;
+
+	public $secret;
+
+	public $region;
+
+	public $version;
+
+	public $bucket;
+
+	private $filesystem;
+
+	
+	public function init()
+	{
+		if($this->endpoint === null){
+			throw new InvalidConfigException('"endpoint" property must be set.');
+		}
+
+		if($this->key === null){
+			throw new InvalidConfigException('"key" property must be set.');
+		}
+
+		if($this->secret === null){
+			throw new InvalidConfigException('"secret" property must be set.');
+		}
+
+		if($this->region === null){
+			throw new InvalidConfigException('"region" property must be set.');
+		}
+
+		if($this->version === null){
+			throw new InvalidConfigException('"version" property must be set.');
+		}
+
+		if($this->bucket === null){
+			throw new InvalidConfigException('"bucket" property must be set.');
+		}
+
+		// build S3 Configuration
+		$s3Config = [
+			'endpoint' => $this->endpoint,
+			'credentials' => [
+				'key' => $this->key,
+				'secret' => $this->secret,
+			],
+			'region' => $this->region,
+			'version' => $this->version,
+		];
+
+
+		// init Filesystem Adapter
+		$s3Client = new S3Client($s3Config);
+		$adapter = new AwsS3Adapter($s3Client, $this->bucket);
+		$this->filesystem = new Filesystem($adapter);
+
+		parent::init();
+	}
+
+
+	public function readFile($filename)
+	{
+		if($filename === null){
+			throw new InvalidArgumentException('filename argument must be passed.');
+		}
+
+		if(!$this->filesystem->has($filename)){
+			throw new NotFoundHttpException("file not found.");
+		}else{
+			$stream = $this->filesystem->readStream($filename);
+			$mimetype = $this->filesystem->getMimetype($filename);
+
+			return [
+				'mimetype' => $mimetype,
+				'stream' => $stream
+			];
+		}
+	}
+
+
+	public function writeFile($fileInstance, $filename='')
+	{
+		if($fileInstance === null){
+			throw new InvalidArgumentException('fileInstance argument must be passed.');
+		}
+
+		if(($fileInstance instanceof \yii\web\UploadedFile) == false){
+			throw new InvalidArgumentException('fileInstance argument must be instance of yii\web\UploadedFile.');
+		}
+
+		$writeFilename = $filename ?? $file->name;
+
+		if($fileInstance->error === UPLOAD_ERR_OK && !$this->filesystem->has($writeFilename)){
+
+			$stream = fopen($fileInstance->tempName, 'r+');
+			$this->filesystem->writeStream($writeFilename, $stream);
+			fclose($stream);
+
+			return true;
+		}else{
+			return false;
+		}
+	}
+
+
+	public function deleteFile($filename)
+	{
+		if($filename === null){
+			throw new InvalidArgumentException('filename argument must be passed.');
+		}
+
+		if(!$this->filesystem->has($filename)){
+			throw new NotFoundHttpException("file not found.");
+		}else{
+			return $this->filesystem->delete($filename);
+		}
+	}
+}
